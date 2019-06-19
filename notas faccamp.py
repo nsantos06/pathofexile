434edda945c1317d11370c9bf9e81da95e8c1fea
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2019-06-19 01:21:36 -0300

    Update notas faccamp.py

--- notas faccamp.py
+++ notas faccamp.py
@@ -1,3 +1,4 @@
+#calcular as notas de forma "automatizada"
 import math
 
 #nota dos pbl
@@ -5,13 +6,17 @@ pbl1 = float(input("digita sua primeira nota do pbl: "))
 pbl2 = float(input("digite a segunda nota do pbl: "))
 x = pbl1 + pbl2
 x1 = x / 2
+print("A soma das notas dos pbls é:",x)
 print("A sua media dos pbls é:",x1)
+
 #nota das provas
 p1 = float(input("digita a primeira nota das provas: "))
 p2 = float(input("digita a segunda nota das provas: "))
 y = p1 + p2
 y1 = y / 2
+print("A soma das notas das provas é:",y)
 print("A sua media das provas é:",y1)
+
 #o resultado dos pbls multiplicado o resultado das notas
 xy = x1 * y1
 xy1 = math.sqrt(xy)
