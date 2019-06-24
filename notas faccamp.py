73c8629897c34b9451b16ef605b43e6bae3ccb54
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2019-06-24 02:52:56 -0300

    Update notas faccamp.py

--- notas faccamp.py
+++ notas faccamp.py
@@ -4,24 +4,21 @@ import math
 #nota dos pbl
 pbl1 = float(input("digita sua primeira nota do pbl: "))
 pbl2 = float(input("digite a segunda nota do pbl: "))
-x = pbl1 + pbl2
-x1 = x / 2
-print("A soma das notas dos pbls é:",x)
-print("A sua media dos pbls é:",x1)
+x = (pbl1 + pbl2)/2
+print("A sua media dos pbls é:",x)
 
 #nota das provas
 p1 = float(input("digita a primeira nota das provas: "))
 p2 = float(input("digita a segunda nota das provas: "))
-y = p1 + p2
-y1 = y / 2
-print("A soma das notas das provas é:",y)
-print("A sua media das provas é:",y1)
+y = (p1 + p2)/2
+print("A sua média das provas sao:",y)
 
 #o resultado dos pbls multiplicado o resultado das notas
-xy = x1 * y1
+xy = x * y
 xy1 = math.sqrt(xy)
 print("A sua média total é:",xy1)
 if xy1 >= 5:
   print("Parabéns você está aprovado nesta matéria")
 else:
   print("Errrooou você nao está aprovado nesta matéria")
+#edited in 24/06 2:52 PM
