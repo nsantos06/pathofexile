5f287b91fd01b6007b7b88f2fff6359438fb4b62
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2019-06-24 16:49:09 -0300

    Update pdps.py

--- pdps.py
+++ pdps.py
@@ -1,7 +1,6 @@
 p1 = float(input("digite o minimo de ataque: "))
 p2 = float(input("digite o maximo de ataque: "))
 atk = float(input("Digite o atk speed da arma: "))
-soma = p1 + p2 
-soma2 = soma / 2
-soma3 = soma2 * atk
-print(soma3)
+soma = (p1 + p2)/2*(atk)
+print(soma)
+#edited in 24/06 4.49 pm
