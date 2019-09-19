0a0b414cf69a159a88831c39d6dc0e5f766fa9e4
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2019-09-18 22:13:58 -0300

    Create projeto2lab.c

---
+++ projeto2lab.c
@@ -0,0 +1,22 @@
+// nao terminado
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	//conversao de fahreinheit para celsius e de , kelvin para celsius
+  
+//conversor de  fahrenheit pra  celsius
+  float f;
+  printf("digite o valor em fahrenheit: ");
+  scanf("%f",&f);
+  float c = (f - 32) * 5 / 9;
+  printf("A conversao de fahrenheit pra celsius sera de: %f",c);
+//fim
+
+//conversor de kelvin para celsius
+
+	
+	
+	
+	return 0;
+}
