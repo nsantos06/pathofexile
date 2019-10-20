e88347302dfd4499e0f2e45797ebb0bd7ecfc458
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2019-10-20 03:06:12 -0300

    Update switchcase2.c

--- switchcase2.c
+++ switchcase2.c
@@ -2,25 +2,26 @@
 
 int main(void) {
   int nota1,nota2,media;
+
   printf("Digite a primeira nota : \n ");
   scanf("%d",&nota1);
   printf("Digite a segunda nota : \n ");
   scanf("%d",&nota2);
   media = (nota1 + nota2) / 2 ;
+  if(media <= 4) media = 1;
+  if(media > 4.1 && media <= 7) media = 2;
+  if(media > 7.1 && media <= 10 ) media = 3;
   switch(media){
     case 1:
-    if(media <= 4){ 
-    printf("Reprovado");}
+    printf("Reprovado");
     break;
     
     case 2:
-    if(media > 4.1 && media <= 7){ 
-    printf("Exame");}
+    printf("Exame");
     break;
     
     case 3:
-    if(media > 7.1 && media <= 10 ){
-    printf("Aprovado");}
+    printf("Aprovado");
     break;
     
     default:
