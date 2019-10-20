908b18eaa63745c980fa71746fe392372c2a9e11
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2019-10-20 02:37:42 -0300

    Create switchcase.c

---
+++ switchcase.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main(void) {
+  int codigo;
+  printf("Digite o codigo do produto : \n ");
+  scanf("%d",&codigo);
+  switch(codigo){
+    case 1:
+    printf("Alimento nao perecivel \n ");
+    break;
+    case 2:
+    printf("Alimento perecivel \n ");
+    break;
+    case 3:
+    printf("Alimento perecivel \n ");
+    break;
+    case 4:
+    printf("Alimento perecivel \n ");
+    break;
+    case 5:
+    printf("Vestuario \n ");
+    break;
+    case 6:
+    printf("Vestuario \n ");
+    break;
+    case 7:
+    printf("Higiene pessoal \n ");
+    break;
+    case 8:
+    printf("Limpeza e Utensilios domesticos \n ");
+    break;
+    case 9:
+    printf("Limpeza e Utensilios domesticos \n ");
+    break;
+    case 10:
+    printf("Limpeza e Utensilios domesticos \n ");
+    break;
+    case 11:
+    printf("Limpeza e Utensilios domesticos \n ");
+    break;
+    case 12:
+    printf("Limpeza e Utensilios domesticos \n ");
+    break;
+    case 13:
+    printf("Limpeza e Utensilios domesticos \n ");
+    break;
+    case 14:
+    printf("Limpeza e Utensilios domesticos \n ");
+    break;
+    case 15:
+    printf("Limpeza e Utensilios domesticos \n ");
+    break;
+    default:
+    printf("Codigo invalido \n ");
+    break;
+  }
+
+  return 0;
+}
