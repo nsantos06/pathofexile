1babe54ab56132566ac1e1e5ee278e9faee382e2
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-03-09 01:31:41 -0300

    Create pastas.py

---
+++ pastas.py
@@ -0,0 +1,9 @@
+#criador de pastas(diretorios)
+import os 
+
+n = int(input("Qnts pastas?:"))
+for c in range(0,n):
+    k = str(input("Nome:"))
+    def pastas():
+        create = os.mkdir(k)
+    pastas()
