183e258087a967df910c8ea6bf318c52b995f8bf
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-03-09 20:15:23 -0300

    Update exercicio9,2020.c

--- exercicio9,2020.c
+++ exercicio9,2020.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-	int idade,j,n;
-	int media,idade2;
+	int idade,j,idade2=0;
+	int media = 0;
+	int n = 0;
 	
 	printf("Quantas vezes quer repetir?");
 	scanf("%d",&n);
-	
+		
 	for(j = 0; j<n; j++){
 		printf("digite a idade:");
 		scanf("%d",&idade);
-		idade2+=idade;
+		if((n-1) != j){
+		idade2+=idade;	
+		}
+		
 	}
-	media = idade2 / n;
+	media = idade2 / (n-1);
 	printf("%d",media);
 	
 	
