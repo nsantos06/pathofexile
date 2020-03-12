51e71139c064bb44332011a304066ee0b6e917a7
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-03-12 16:41:35 -0300

    Update binario_e_decimal.py

--- binario_e_decimal.py
+++ binario_e_decimal.py
@@ -1,15 +1,17 @@
-#função binario pra decimal
+#seletor
 rep = int(input("Qual deseja executar? 1 de bin pra decimal, 2 decimal pra binario:"))
+
+#função binario pra decimal
 if rep == 1:
   def bin():
       x = 0
       b = 1
       k = int(input("Digite o numero em binario:"))
       while k !=0:
-        j = k % 10
-        x = x + (j*b)
-        k = k//10
-        b = b*2
+        j = k % 10 # pego o valor inserido e faço o resto da divisao por 10
+        x = x + (j*b) # x vai armazenar o resto da divisão multiplicado pela base(2)
+        k = k//10 # pego o valor inserido e divido por dez
+        b = b*2 # b seria base
     print("O Numero em decimal vai ser:",x)
 bin()
 
