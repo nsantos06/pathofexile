8c7bed85b4393c43b7f7bc6bc566ab34dc7795c5
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-03-17 14:09:24 -0300

    Delete pdps.py

--- pdps.py
+++
@@ -1,6 +0,0 @@
-p1 = float(input("digite o minimo de ataque: "))
-p2 = float(input("digite o maximo de ataque: "))
-atk = float(input("Digite o atk speed da arma: "))
-soma = (p1 + p2)/2*(atk)
-print(soma)
-#edited in 24/06 4.49 pm
