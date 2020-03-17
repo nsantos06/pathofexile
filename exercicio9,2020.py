f783a8ce631486f2e53c6f7d20016071a3d64755
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-03-17 14:10:17 -0300

    Delete exercicio9,2020.py

--- exercicio9,2020.py
+++
@@ -1,9 +0,0 @@
-n = 0
-k = int(input("qnts vezes vai repetir :"))
-for c in range(0,k):
-  idade = int(input("Digite a idade:"))
-  if (k-1) != c:
-    n = n + idade
-
-media = n/(k-1)
-print(media)
