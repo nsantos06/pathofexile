3992411f3bc6bf60227037941192e99e978a270c
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-03-17 14:09:31 -0300

    Delete pastas.py

--- pastas.py
+++
@@ -1,9 +0,0 @@
-#criador de pastas(diretorios)
-import os 
-
-n = int(input("Qnts pastas?:"))
-for c in range(0,n):
-    k = str(input("Nome:"))
-    def pastas():
-        create = os.mkdir(k)
-    pastas()
