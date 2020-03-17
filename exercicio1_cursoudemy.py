ac8153881f0b13b5559eb4a4712525a7d7ee8f65
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-03-17 14:10:53 -0300

    Delete exercicio1_cursoudemy.py

--- exercicio1_cursoudemy.py
+++
@@ -1,7 +0,0 @@
-x = [] #lista
-cont = 0 #contador inicia em zero
-for i in range(0,7): # for de 0 a 7 pra ser repetido 7 vezes
-  x.append(float(input("Digita o peso:"))) # adicionando numeros na lista 
-  if x[i] > 90: # se x na posicao I for maior que 90
-    cont += 1 
-print(cont,sum(x)/len(x)) # sum = somando todos os valores de x, len x = contador de x
