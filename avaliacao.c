107e303b41d13ac8d36abb1e1d02fe3d307ced01
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-04-21 13:54:00 -0300

    Create avaliacao.c

---
+++ avaliacao.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void) {
+ int votos,vt;
+ int n,i,contvotos=0;
+ 
+printf("Quantas vezes vai votar?:");
+scanf("%d",&i);
+
+printf("Digite em quem quer votar (1,2,3 ou 4):");
+scanf("%d",&votos);
+
+ 
+ for(n = 0; n<i; n++){
+  switch(votos){
+    case 1:
+    printf("Você votou no candidato 1 \n");
+    break;
+   
+    case 2:
+    printf("Você votou no candidato 2 \n");
+    break;
+   
+    case 3:
+    printf("Você votou no candidato 3 \n");
+    break;
+   
+    case 4:
+    printf("Você votou no candidato 4 \n");
+    break;
+
+    default:
+    printf("Valor invalido");
+  
+ }
+ 
+ }
+printf("%d \n",n);
+  return 0;
+}
