72b0e34b2253ad30ca1461e6b7399eb33bd1ab6d
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-05-18 21:40:03 -0300

    Update atividadeemail.c

--- atividadeemail.c
+++ atividadeemail.c
@@ -1,34 +1,37 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+exercicio 2
+int main(){
 
-int main(void) {
-  int k,r,linhas,colunas;
-  int cont = 0;
+int linhas,colunas,k,r;
+int cont=0;
 
-  printf("Linhas:");
-  scanf("%d",&linhas);
+printf("Linhas: ");
+scanf("%d",&linhas);
 
-  printf("colunas:");
-  scanf("%d",&colunas);
-  
-  int matriz[linhas][colunas] = {}; 
-  
-  for(k=0; k<linhas; k++){
-    for(r=0; r<colunas; r++){
-      printf("Elementos na linha %d e coluna %d:",k+1, r+1);
-      scanf("%d",&matriz[k][r]);
-    }
-    printf("\n");
-  }
+printf("\nColunas: ");
+scanf("%d",&colunas);
 
- for(k=0; k<linhas; k++){
-    for(r=0; r<colunas; r++){
-      if(matriz[k][r] < 0 || matriz[k][r] > 100){
-        cont++;
-      }
-    }
- }
-  printf("\n Valores fora do intervalo [0,100] na linha %d = %d",k+1,cont);
-cont=0;
-  return 0;
+int matriz[linhas][colunas];
+for(k = 0; k<linhas; k++){
+  for(r=0 ; r<colunas;r++){
+  printf("Informe o numero a ser armazenado na linha %d coluna %d: ", k+1,r+1);
+  scanf("%d", &matriz[k][r]);
 }
+printf("\n");
+}
+for(k = 0; k<linhas;k++){
+  for(r=0;r<colunas;r++){
+  if(matriz[k][r] < 0 || matriz[k][r] > 100){
+    cont++;
+}
+}
+printf("\nValores fora do intervalo [0,100] na linha %d = %d", k+1 , cont);
+cont = 0;
+
+}
+return 0;
+}
+
+
