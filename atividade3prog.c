43413641d2311795dc550536b249370e115745e6
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-06-08 22:48:14 -0300

    Update atividade3prog.c

--- atividade3prog.c
+++ atividade3prog.c
@@ -23,6 +23,39 @@ int main(void) {
   return 0;
 }
 
+//atividade 2
+#include <stdio.h>
+
+int main(void) {
+  char cidades[100][100];
+  float dados[4][0];
+  int i,k,j;
+  
+  printf("Digite a quantidade de cidades:");
+  scanf("%d",&k);
+
+  for(i = 0; i < k; i++){
+    printf("Digite o nome das cidade: \n");
+    scanf("%s",cidades[i]);
+  }
+  
+  for(i = 0; i<k; i++){
+    printf("Digite quantos graus na cidade de %s: \n",cidades[i]);
+    scanf("%f",dados[i]);
+}
+  for(i = 0; i<k; i++){
+    printf("Digite a umidade na cidade de %s: \n",cidades[i]);
+    scanf("%f",dados[i]);
+}
+
+  for(i = 0; i<k; i++){
+    printf("Digite o vento em km/h na cidade de %s: \n",cidades[i]);
+    scanf("%f",dados[i]);
+}
+
+
+  return 0;
+}
 
 
 
@@ -41,7 +74,7 @@ int main(void) {
 
 
 
-
+//atividade 6
 
 #include <stdio.h>
 
