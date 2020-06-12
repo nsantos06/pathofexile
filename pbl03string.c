d01e0ad20decf927f7469d12f110c497649ba421
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-06-12 00:04:14 -0300

    Update pbl03string.c

--- pbl03string.c
+++ pbl03string.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 int main(void) {
   int k, contador = 0, i = 0;
-  
+  char maior,menor;
   char s1[100];
   char s2[100];
 
@@ -11,14 +12,16 @@ int main(void) {
   
   printf("Digite a segunda string:");
   scanf("\n%[^\n]",s2);
+  
+
 
   int k1 = strlen(s1);
   int k2 = strlen(s2);
   
   for(k = 0; k<k1; k++)
   {
-    
-    if(s1[k] == s2[i]){
+   
+    if(toupper(s1[k]) == toupper(s2[i])){
       
       if(i == (k2-1)){
         contador++;
@@ -28,7 +31,9 @@ int main(void) {
    else{
         i = 0;
       }
+    
   }
   printf("contador :[%d]\n",contador);
+  
   return 0;
 }
