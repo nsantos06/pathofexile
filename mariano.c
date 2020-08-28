d0b338c3431095cb4090b4cb4b3f4fdf2ffee302
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-08-28 00:38:38 -0300

    Update mariano.c

--- mariano.c
+++ mariano.c
@@ -25,3 +25,33 @@ int main(void) {
 
   return 0;
 }
+
+exercicio2 - meio errado
+#include <stdio.h>
+
+int main(void) {
+  int i,tam,aux,aux2;
+  printf("Digite o tamanho do vetor:");
+  scanf("%d",&tam);
+
+  int vetor[tam],arr[tam];
+  for(i = 0; i< tam; ++i){
+    printf("Digite os numeros:");
+    scanf("%d",&vetor[i]);
+    aux = vetor[i];
+  }
+
+  for(i = 0; i<tam; ++i){
+    printf("Digite os numeros no segundo vetor:");
+    scanf("%d",&arr[i]);
+    aux2 = arr[i];
+  }
+
+  if(aux == aux2){
+      printf("Sao iguais!");
+    }else{
+      printf("Sao diferentes!");
+    }
+
+  return 0;
+}
