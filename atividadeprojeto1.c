5a32dc0139be55afa7030f60b847a5e670ff8b16
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-10-05 22:01:19 -0300

    Update atividadeprojeto1.c

--- atividadeprojeto1.c
+++ atividadeprojeto1.c
@@ -83,3 +83,162 @@ for( i = 0; i<disciplina; i++){
 
   return 0;
 }
+
+
+Exercicio 2, Letra A
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <locale.h>
+
+#define qtdalunos 2
+#define materia 3
+
+
+
+
+void ordemVetor(int *vetor, int tamanho) {
+	int auxiliar; 
+	int i, c;
+	
+	for (i = 1; i < tamanho; ++i) { 
+	   for (c = 1; c < tamanho; ++c) {
+	      if (vetor[c-1] > vetor[c]) {
+          auxiliar = vetor[c-1];
+          vetor[c-1] = vetor[c];
+          vetor[c] = auxiliar;
+	      }
+	   }
+	}
+	
+}
+
+int pegarString(char *vetor) {
+	int x;
+	int y = 0;
+	
+	for (x = 0; x < strlen(vetor); ++x) {
+		if (!isalpha(vetor[x])) {
+			if(!isspace(vetor[x])) {
+				++y;
+			}
+		}
+	}
+	return y > 0 ? 0 : 1;
+}
+
+
+int pegarNumero(char *vetor) {
+	int x;
+	int y = 0;
+
+	for (x = 0; x < strlen(vetor); ++x) {
+		if (isalpha(vetor[x])) {
+			if(vetor[x] != ',') {
+				++y;
+			}
+		}
+	}
+	
+	return y > 0 ? 0 : 1;
+}
+
+int main() {
+	
+	int i, k; 
+	
+  char estudante_nome[qtdalunos][50]; 
+	char entrada[50]; 
+	int Ra[qtdalunos]; 
+	float materias[qtdalunos][materia]; 
+	
+  int n;
+  
+	for (i = 0; i < qtdalunos; ++i) {
+	  printf("\nDigite o nome do estudante [%d]: ", i);
+		scanf("\n%[^\n]", estudante_nome[i]);
+		
+	  printf("Digite a matricula do estudante [%d]: ", i);
+		scanf("%s", entrada);
+		
+	
+		
+		Ra[i] = atoi(entrada);
+		
+		for (k = 0; k < materia; ++k) {
+			printf("Digite a nota que o aluno %s obteve no curso [%d]: ", estudante_nome[i], k+1);
+			scanf("%s", entrada);
+			
+			n = atof(entrada);
+		  materias[i][k] = atof(entrada);
+		}
+	}
+	
+	int x = 0;
+
+	for (i = 0; i < qtdalunos; ++i) {
+		int s = 0;
+		for (k = 0; k < materia; ++k) {
+			if(materias[i][k] >= 40) {
+				s += materias[i][k];		
+			}
+		}
+
+		if (s >= 150) {
+			++x;
+		}
+	}
+
+	int aproved[x];
+	int l = 0;
+	
+	for (i = 0; i < qtdalunos; ++i) {
+		int s = 0;
+		for (k = 0; k < materia; ++k) {
+			if(materias[i][k] >= 40) {
+				s += materias[i][k];		
+			}
+		}
+
+		if (s >= 150) {
+			aproved[l] = i;
+			++l;
+		}
+	}
+	
+
+	int aux; 
+	int s1 = 0, s2 = 0;
+	
+	for (i = 1; i < x; ++i) { 
+	   for (l = 1; l < x; ++l) {
+	   	
+		for (k = 0; k < materia; ++k) {
+				s1 += materias[l][k];	
+		}
+		
+		for (k = 0; k < materia; ++k) {
+				s2 += materias[l-1][k];	
+		}
+		
+	      if (aproved[l-1] < aproved[l]) {
+          aux = aproved[l-1];
+          aproved[l-1] = aproved[l];
+          aproved[l] = aux;
+	      }
+	   }
+	}
+
+	for (i = 0; i < x; ++i) {
+		int ap = aproved[i];
+		int s = 0;
+
+		printf("\n%d. Candidado [%s] inscrito pela matricula [%d] foi Aprovado", i+1, estudante_nome[ap], Ra[ap]);
+		
+	}		
+  return 0;
+}
+
